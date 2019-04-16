817a13c2b08ccb8086ac504fbcd17860ce673cd6
kawochan <warren.kw.chan@alumni.ubc.ca>
2019-04-15 21:38:51 -0700

    Initial Commit

---
+++ Firebase Demo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Firebase Demo
+//
+//  Created by Warren Chan on 2019-04-15.
+//  Copyright © 2019 Warren Chan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Firebase Demo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Firebase Demo
+//
+//  Created by Warren Chan on 2019-04-15.
+//  Copyright © 2019 Warren Chan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
